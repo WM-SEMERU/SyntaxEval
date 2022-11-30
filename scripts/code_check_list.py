3f4fceb4e0af40b8c042dff870632e7b1d5ea0f2
Alejandro Velasco <svelascodimate@wm.edu>
2022-11-30 16:02:34 -0500

    fized filtering error

--- scripts/code_check_list.py
+++ scripts/code_check_list.py
@@ -6,7 +6,7 @@ import CodeCheckList.utils as utils
 
 ########## YOU NEED TO SET THIS FIRST #######
 checkpoint = "huggingface/CodeBERTa-small-v1"
-number_of_samples = 10000
+#number_of_samples = 10000
 number_of_predictions_per_sample = 3
 python_language = "python"
 save_path = "output/"
@@ -19,11 +19,8 @@ evaluator = Evaluator(checkpoint, python_language)
 
 max_token_number = evaluator.tokenizer.tokenizer.max_len_single_sentence
 
-test_set = load_dataset("code_search_net", split='test')
-test_set = test_set.filter(lambda sample: True if sample['language']== python_language
-            and len(sample['func_code_tokens']) <= max_token_number
-            and len(evaluator.tokenizer.tokenizer(sample['whole_func_string'])['input_ids']) <= max_token_number else False, num_proc=1)
-#test_set = utils.get_sub_set_test_set(test_set, number_of_samples)
+#test_set = utils.get_random_sub_set_test_set(utils.get_test_sets(load_dataset("code_search_net", split='test'), python_language, evaluator.tokenizer.tokenizer.max_len_single_sentence, evaluator.tokenizer), number_of_samples)
+test_set = utils.get_test_sets(load_dataset("code_search_net", split='test'), python_language, evaluator.tokenizer.tokenizer.max_len_single_sentence, evaluator.tokenizer)
 
 results_dataframe = evaluator(test_set, number_of_predictions_per_sample)
 results_dataframe = results_dataframe.sort_values(by=['occurences'], ascending=False)
--- scripts/commands.md
+++ scripts/commands.md
@@ -2,6 +2,7 @@
 ## processes
 
 nohup python3 code_check_list.py > logs/huggingface-CodeBERTa-small-v1_10_3.txt &
+nohup python3 code_check_list.py > logs/huggingface-CodeBERTa-small-v1_50_3.txt &
 nohup python3 code_check_list.py > logs/huggingface-CodeBERTa-small-v1_100_3.txt &
 nohup python3 code_check_list.py > logs/huggingface-CodeBERTa-small-v1_1000_3.txt &
 nohup python3 code_check_list.py > logs/huggingface-CodeBERTa-small-v1_10000_3.txt &
