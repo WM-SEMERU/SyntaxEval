a07322b7f6af88aa71764c5f7456d8d5d55a5973
Alejandro Velasco <svelascodimate@wm.edu>
2022-12-09 18:51:50 -0500

    updated scripts

--- scripts/code_check_list.py
+++ scripts/code_check_list.py
@@ -6,10 +6,10 @@ import CodeCheckList.utils as utils
 
 ########## YOU NEED TO SET THIS FIRST #######
 checkpoint = "huggingface/CodeBERTa-small-v1"
-number_of_samples = 50
+number_of_samples = 100
 number_of_predictions_per_sample = 3
 masking_rate = 1
-gpu_available = True
+gpu_available = False
 python_language = "python"
 save_path = "output/"
 ########## YOU NEED TO SET THIS FIRST #######
@@ -17,26 +17,26 @@ save_path = "output/"
 
 languages = [python_language]
 loader.download_grammars(languages)
-evaluator = Evaluator(checkpoint, python_language)
 
+################ DEFINE EVALUATOR 
+evaluator = Evaluator(checkpoint, python_language, gpu_available)
 max_token_number = evaluator.tokenizer.tokenizer.max_len_single_sentence
 
-print('---STARTED EVALUATION----')
+print('--- EVALUATION STARTED ----')
 
-################ EVALUATOR
-test_set = utils.get_random_sub_set_test_set(utils.get_test_sets(load_dataset("code_search_net", split='test'), python_language, evaluator.tokenizer.tokenizer.max_len_single_sentence, evaluator.tokenizer), number_of_samples, gpu_available)
-#test_set = utils.get_test_sets(load_dataset("code_search_net", split='test'), python_language, evaluator.tokenizer.tokenizer.max_len_single_sentence, evaluator.tokenizer, gpu_available)
+################ TEST SET
+test_set = utils.get_random_sub_set_test_set(utils.get_test_sets(load_dataset("code_search_net", split='test'), python_language, evaluator.tokenizer.tokenizer.max_len_single_sentence, evaluator.tokenizer), number_of_samples)
+#test_set = utils.get_test_sets(load_dataset("code_search_net", split='test'), python_language, evaluator.tokenizer.tokenizer.max_len_single_sentence, evaluator.tokenizer)
 
+
+################ CALL EVALUATOR
 results_dataframe = evaluator(test_set, number_of_predictions_per_sample, masking_rate)
 results_dataframe = results_dataframe.sort_values(by=['occurences'], ascending=False)
 
 print(results_dataframe.head())
 
-print('---END EVALUATION----')
-print('---SAVING TABLE----')
+print('--- EVALUATION FINISHED ---')
 
 ################ OUTPUT
-results_dataframe.to_csv(save_path+checkpoint.replace("/","-")+"_"+str(number_of_samples)+"_"+str(number_of_predictions_per_sample)+".csv")
-#results_dataframe.to_csv(save_path+checkpoint.replace("/","-")+"_"+"all"+"_"+str(number_of_predictions_per_sample)+".csv")
-
-print('---FINISH :D----')
+results_dataframe.to_csv(save_path+checkpoint.replace("/","-")+"_"+str(number_of_samples)+"_"+str(masking_rate*100)+"_"+str(number_of_predictions_per_sample)+".csv")
+#results_dataframe.to_csv(save_path+checkpoint.replace("/","-")+"_"+str(masking_rate*100)+"_"+"all"+"_"+str(number_of_predictions_per_sample)+".csv")
